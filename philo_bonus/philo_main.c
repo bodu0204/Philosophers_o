a4a17ba46a97cab8645c8c9693f7cec03e920c47
Bodu Lyu <blyu@c1r17s1.42tokyo.jp>
2022-07-17 13:57:27 +0900

    commit form makefile

--- philo_bonus/philo_main.c
+++ philo_bonus/philo_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo_main.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryoakira <ryoakira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: blyu <blyu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/11 15:22:08 by blyu              #+#    #+#             */
-/*   Updated: 2022/07/15 11:28:22 by ryoakira         ###   ########.fr       */
+/*   Created: 2022/07/15 13:21:24 by ryoakira          #+#    #+#             */
+/*   Updated: 2022/07/16 16:41:38 by blyu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +27,15 @@ void	*philo(void *vp)
 		if (thinking(p, &s))
 			return ((void *)DEAD);
 		philolog(p, EAT);
-		if (eating(p))
+		if (eating(p, &s))
 			return ((void *)DEAD);
 		philolog(p, SLEEP);
-		if (sleeping(p))
+		if (sleeping(p, &s))
 			return ((void *)DEAD);
 		philolog(p, THINK);
 	}
 	p->status = DEAD;
 	return ((void *)ALIVE);
-}
-
-void	get_schedule(t_info *i, t_schedule	*s)
-{
-	struct timezone	buff;
-	struct timeval	now;
-
-	gettimeofday(&now, &buff);
-	s->et = (now.tv_usec % S_US) + ((now.tv_sec % S_US) * S_US);
-	s->dt = s->et + (i->d * S_US);
-	return ;
 }
 
 int	thinking(t_philo *p, t_schedule	*s)
@@ -56,16 +45,16 @@ int	thinking(t_philo *p, t_schedule	*s)
 
 	if (p->info->control != EXEING)
 		return (DEAD);
-	old_dt = s->et;
+	old_dt = s->dt;
 	f = NEXIST;
 	get_schedule(p->info, s);
 	while (old_dt > s->et && f == NEXIST)
 	{
-		if (p->info->n > 1)
+		if (p->lf.s == EXIST && p->next->lf.s == EXIST)
 			f = rubfork(p);
 		get_schedule(p->info, s);
 	}
-	if (old_dt > s->et)
+	if (f == NEXIST)
 	{
 		p->status = DEAD;
 		philolog(p, DIE);
@@ -74,16 +63,21 @@ int	thinking(t_philo *p, t_schedule	*s)
 	return (ALIVE);
 }
 
-int	eating(t_philo *p)
+int	eating(t_philo *p, t_schedule	*s)
 {
+	unsigned long	n;
+
 	if (p->info->control != EXEING)
-		return (DEAD);
-	p->eat++;
-	if (p->info->d > p->info->e)
-		usleep((p->info->e * S_MS) - TWEAK);
-	else
 	{
-		usleep((p->info->d * S_MS) - TWEAK);
+		p->status = DEAD;
+		return (DEAD);
+	}
+	p->eat++;
+	n = now();
+	while (s->dt > n && s->st > n)
+		n = now();
+	if (s->dt < n)
+	{
 		p->status = DEAD;
 		philolog(p, DIE);
 	}
@@ -96,20 +90,23 @@ int	eating(t_philo *p)
 	return (p->status);
 }
 
-int	sleeping(t_philo *p)
+int	sleeping(t_philo *p, t_schedule	*s)
 {
+	unsigned long	n;
+
 	if (p->info->control != EXEING)
+	{
+		p->status = DEAD;
 		return (DEAD);
-	if (p->info->d > p->info->e + p->info->s)
-	{
-		usleep((p->info->s * S_MS) - TWEAK);
-		return (ALIVE);
 	}
-	else
+	n = now();
+	while (s->dt > n && s->tt > n)
+		n = now();
+	if (s->dt < n)
 	{
-		usleep((p->info->d * S_MS) - TWEAK);
 		p->status = DEAD;
 		philolog(p, DIE);
 		return (DEAD);
 	}
+	return (ALIVE);
 }
